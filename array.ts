00ad4c37995aaa120cea5f1eaf18b144e020fadd
Madhawa-Tharindu <madhawath@gmail.com>
2025-03-19 13:34:22 +0530

    learnt about Arrays in TS

---
+++ array.ts
@@ -0,0 +1,20 @@
+//Arrays
+
+let myArray: number[] = [1,2,3,4];
+let myStringsArray: string[] = ["Apple", "Banana"];
+
+// Mixed Arrays
+
+let mixedArrays: (string | number | boolean)[] = ["Apple", "Banana", 4, 3, true, false];
+
+//Nested Arrays
+
+let arrOfArrays: number[][] = [
+    [1,2,3],
+    [4,5,6]
+]
+
+//Fixed Tuple Arrays
+
+let fixedArrays: [string, number, boolean] = ["Apple", 4, true]
+
