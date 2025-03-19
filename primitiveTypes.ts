5783f11269428f6803aae55becb874630677b9f0
Madhawa-Tharindu <madhawath@gmail.com>
2025-03-19 11:58:23 +0530

    learnt about primitive types

---
+++ primitiveTypes.ts
@@ -0,0 +1,10 @@
+//Primitive data types
+
+//let myName = "Sanoj"; // this is a normal JavaScript variable value
+
+let myName2: string = "Madhawa"; // this is a typescript version
+let myAge: number = 27; 
+let myBoolean: boolean = true;
+
+ let myUndefined: undefined = undefined;
+ let myNull: null = null;
